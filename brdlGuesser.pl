c066bbc94c671578faa4bf94afafa92429c0a607
Terry Nightingale <tnight@pobox.com>
2024-08-20 16:42:53 -0700

    Enhanced the Perl script with documentation.

--- brdlGuesser.pl
+++ brdlGuesser.pl
@@ -9,9 +9,16 @@ sub main() {
   my $fileHandle = undef;
   my $matchCount = 0;
   my $pattern = '\w{4}';
-  # my $speciesFilename = 'ABA_Checklist-8.15.csv';
-  my $speciesFilename = 'short.csv';
-  my $usage = "usage: $0 [pattern]";
+  my $speciesFilename = 'ABA_Checklist-8.15.csv';  # The full data file.
+  # my $speciesFilename = 'short.csv';  # A small data file for testing.
+  my $usage = <<END;
+usage: $0 [pattern]
+
+Search for a four-letter Alpha code using a regular expression search pattern.
+If no pattern is supplied, all four-letter Alpha codes will match.
+
+Example: $0 'R\\wBU'
+END
 
   if ($argCount != 0 && $argCount != 1) {
     print STDERR "$usage\n";
@@ -26,7 +33,7 @@ sub main() {
        "< $encoding",
        $speciesFilename
       )
-    || die "$0: can't open $speciesFilename for reading: $!";
+    || die("$0: can't open $speciesFilename for reading: $!");
 
   while (<$fileHandle>) {
     # Remove the newline for more convenient processing and printing.
@@ -39,18 +46,12 @@ sub main() {
     }
   }
   if ($!) {
-    die "$0: unexpected error while reading from $speciesFilename: $!";
+    die("$0: unexpected error while reading from $speciesFilename: $!");
   }
 
-
-
-  # More sophisticated example:
-
-  # perl -ne 'if (m#,(R\wBU),\d+$#) { printf "%4d. %s\n", ++$count, $1; }' ABA_Checklist-8.15.csv
-
-
+  return $matchCount > 0 ? 0 : 1;
 }
 
-main();
+exit main();
 
 # End of script.
