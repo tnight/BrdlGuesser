d7e42c5394f1264abc807ad56789b2b2a47b9fa1
Terry Nightingale <tnight@pobox.com>
2024-08-20 16:49:09 -0700

    Enhanced Perl one-liner command script with the option to use the small data file or full data file.

--- perlCmd.sh
+++ perlCmd.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-perl -ne 'if (m#,\w+,\d+$#) { print; }' short.csv
+export speciesFilename='ABA_Checklist-8.15.csv';  # The full data file.
+# export speciesFilename='short.csv';  # A small data file for testing.
+
+perl -ne 'if (m#,\w+,\d+$#) { print; }' $speciesFilename
+
+# End of script
