8c593f406af3ceb688e51d6641d5e62b855bd080
Terry Nightingale <tnight@pobox.com>
2025-03-27 21:30:42 -0700

    First commit of hello-world app using App::Cmd

---
+++ BrdlGuesser.pm
@@ -0,0 +1,9 @@
+package BrdlGuesser;
+
+use strict;
+use warnings;
+use App::Cmd::Setup -app;
+
+1;
+
+# End of module
---
+++ BrdlGuesser/Command/dump.pm
@@ -0,0 +1,15 @@
+package BrdlGuesser::Command::dump;
+
+use strict;
+use warnings;
+use BrdlGuesser -command;
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  print "Would have executed the dump command here...\n";
+}
+
+1;
+
+# end of module
